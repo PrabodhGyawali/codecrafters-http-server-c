9ba9995f8f1e0ff9a8edd38f9ffd654708393ebe
root <root@AlienMaster40>
2024-07-12 00:08:53 +0100

    pass stage

--- app/server.c
+++ app/server.c
@@ -54,6 +54,10 @@ int main() {
 	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
 
+	// Send a response to the client
+	char* response = "HTTP/1.1 200 OK\r\n\r\n";
+	sendmsg(server_fd, (struct msghdr *)response, 0);
+
 	close(server_fd);
 
 	return 0;
